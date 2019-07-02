4274a8395f45149f419e759bc940f7f13b6b00c0
Damien <damien.m.45@gmail.com>
2019-07-02 12:05:06 +0200

    2nd endpoint

--- app/controllers/api/v1/beers_controller.rb
+++ app/controllers/api/v1/beers_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::BeersController < Api::V1::BaseController
-    def index
-       @beers = Beer.all
+    before_action :set_beer, only: [ :show ]
+    def show
     end
+    private
+    def set_beer
+       @beer = Beer.find(params[:id])
+    end
+
   end
